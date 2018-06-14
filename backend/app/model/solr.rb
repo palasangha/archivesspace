9727c5cb11e9d55e011a95273863bfc7e89c9b62
Laney McGlohon <lmcglohon@gmail.com>
2018-06-14 12:30:09 -0700

    Refactor where solr parameters are set

--- backend/app/model/solr.rb
+++ backend/app/model/solr.rb
@@ -245,24 +245,23 @@ class Solr
       if @show_published_only
         add_solr_param(:fq, "publish:true")
         add_solr_param(:fq, "types:pui")
-<<<<<<< HEAD
-        if AppConfig[:solr_params].any?
-          if !AppConfig[:solr_params].has_key? :qf
-            add_solr_param(:qf, pui_qf)
-          end
-        else
-          add_solr_param(:qf, pui_qf)
-        end
+      #   if AppConfig[:solr_params].any?
+      #     if !AppConfig[:solr_params].has_key? :qf
+      #       add_solr_param(:qf, pui_qf)
+      #     end
+      #   else
+      #     add_solr_param(:qf, pui_qf)
+      #   end
+      # else
+      #   if AppConfig[:solr_params].any?
+      #     if !AppConfig[:solr_params].has_key? :qf
+      #       add_solr_param(:qf, staff_ui_qf)
+      #     end
+      #   else
+      #     add_solr_param(:qf, staff_ui_qf)
+      #   end
       else
-        if AppConfig[:solr_params].any?
-          if !AppConfig[:solr_params].has_key? :qf
-            add_solr_param(:qf, staff_ui_qf)
-          end
-        else
-          add_solr_param(:qf, staff_ui_qf)
-        end
-=======
->>>>>>> Enhance PUI search results
+        add_solr_param(:qf, "fullrecord")
       end
 
 
@@ -284,32 +283,27 @@ class Solr
         add_solr_param(:"facet.mincount", @facet_mincount)
       end
 
-      add_solr_param(:"q.op", "AND")
-      add_solr_param(:"mm", "6<-1 6<90%")
-
       if @query_type == :edismax
         add_solr_param(:defType, "edismax")
-<<<<<<< HEAD
-        if AppConfig[:solr_params].any?
-          if !AppConfig[:solr_params].has_key? :"q.op"
-            add_solr_param(:"q.op", "AND")
-          end
-          if !AppConfig[:solr_params].has_key? :mm
-            add_solr_param(:mm, "6<-1 6<90%")
-          end
-          if !AppConfig[:solr_params].has_key? :pf
-            add_solr_param(:pf, "four_part_id^50")
-          end
-          if !AppConfig[:solr_params].has_key? :bq
-            add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
-          end
-        else
-          add_solr_param(:"q.op", "AND")
-          add_solr_param(:mm, "6<-1 6<90%")
-          add_solr_param(:pf, "four_part_id^50")
-          add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
-        end
-=======
+        # if AppConfig[:solr_params].any?
+        #   if !AppConfig[:solr_params].has_key? :"q.op"
+        #     add_solr_param(:"q.op", "AND")
+        #   end
+        #   if !AppConfig[:solr_params].has_key? :mm
+        #     add_solr_param(:mm, "6<-1 6<90%")
+        #   end
+        #   if !AppConfig[:solr_params].has_key? :pf
+        #     add_solr_param(:pf, "four_part_id^50")
+        #   end
+        #   if !AppConfig[:solr_params].has_key? :bq
+        #     add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
+        #   end
+        # else
+        #   add_solr_param(:"q.op", "AND")
+        #   add_solr_param(:mm, "6<-1 6<90%")
+        #   add_solr_param(:pf, "four_part_id^50")
+        #   add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
+        # end
         add_solr_param(:pf, "four_part_id^50")
         add_solr_param(:qf, "title^25")
         add_solr_param(:qf, "four_part_id^50")
@@ -320,7 +314,6 @@ class Solr
         add_solr_param(:bq, "primary_type:agent_person^50")
         add_solr_param(:bq, "primary_type:agent_corporate_entity^30")
         add_solr_param(:bq, "primary_type:agent_family^30")
->>>>>>> Enhance PUI search results
       end
 
       # do it here so instance variables can be resolved
